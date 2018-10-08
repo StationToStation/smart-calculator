d9b0b98115bd0e1bd7b06abef981e3d09609e9fa
Anastasiya Garbuz <36642340+StationToStation@users.noreply.github.com>
2018-10-08 17:26:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,80 +1,59 @@
 class SmartCalculator {
-  constructor(startValue) {
+   constructor(startValue) {
     this.result = startValue;
     this.lastAddend = undefined;
     this.lastMultiplier = undefined;
-    this.lastPow = undefined;
+    this.wasPowed = false;
   }
-
-  toString() {
-    return this.result;
-  }
-
-  add(number) {
+   toString() {
+@@ -13,13 +14,15 @@ class SmartCalculator {
     this.result += number;
     this.lastAddend = number;
     this.lastMultiplier = undefined;
-    this.lastPow = undefined;
+    this.wasPowed = false;
     return this;
   }
-
-  subtract(number) {
+   subtract(number) {
     this.result -= number;
     this.lastAddend = -number;
     this.lastMultiplier = undefined;
-    this.lastPow = undefined;
+    this.wasPowed = false;
     return this;
   }
-
-  multiply(number) {
-    if (this.lastAddend === undefined)
-      this.result *= number;
-    else {
-      this.result += this.lastAddend *( number-1);
+ @@ -31,6 +34,7 @@ class SmartCalculator {
       this.lastAddend = this.lastAddend * number;
     }
     this.lastMultiplier = number;
-    this.lastPow = undefined;
+    this.wasPowed = false;
     return this;
   }
-
-  devide(number) {
-    if (this.lastAddend === undefined)
-      this.result /= number;
-    else {
-      this.result = this.result- this.lastAddend +this.lastAddend/number;// (1-number) / number;
+ @@ -42,11 +46,19 @@ class SmartCalculator {
       this.lastAddend = this.lastAddend / number;
     }
     this.lastMultiplier = 1 / number;
-    this.lastPow = undefined;
+    this.wasPowed = false;
     return this;
   }
-
-  pow(number) {
+   pow(number) {
+    if (this.lastMultiplier != undefined) {
+    if (this.wasPowed) {
+      this.wasPowed = true;
+      return this;
+    }
     if (this.lastMultiplier != undefined & this.lastAddend != undefined) {
-      if (this.lastPow!=undefined) {
-        this.result=this.result - this.lastAddend+this.lastAddend/Math.pow(this.lastMultiplier,this.lastPow)*Math.pow(this.lastMultiplier, Math.pow(this.lastPow,number));
-        this.lastMultiplier = Math.pow(this.lastMultiplier, Math.pow(this.lastPow,number)/this.lastPow);
-        this.lastPow*=number;
-      }
-      this.result += this.lastAddend * (Math.pow(this.lastMultiplier, number - 1)-1);
+      this.result = this.result - this.lastAddend + this.lastAddend * Math.pow(this.lastMultiplier, number - 1);
       this.lastMultiplier = Math.pow(this.lastMultiplier, number);
     } else if (this.lastMultiplier != undefined) {
-      this.result *= Math.pow(this.lastMultiplier, number-1);
+      this.result = this.result / this.lastMultiplier * Math.pow(this.lastMultiplier, number);
       this.lastMultiplier = Math.pow(this.lastMultiplier, number);
     } else if (this.lastAddend < 0 & number % 2 == 0) {
-      this.result = this.result - this.lastAddend - Math.pow(this.lastAddend, number);
-      this.lastAddend = - Math.pow(this.lastAddend, number);
-    } else if (this.lastAddend != undefined) {
-      this.result = this.result - this.lastAddend + Math.pow(this.lastAddend, number);
-      this.lastAddend = Math.pow(this.lastAddend, number);
+@@ -58,6 +70,7 @@ class SmartCalculator {
     } else {
       this.result = Math.pow(this.result, number);
     }
-    this.lastPow = number;
+    this.wasPowed = true;
     return this;
   }
-
-}
-
-module.exports = SmartCalculator;
+  }
+ 
+ module.exports = SmartCalculator;
